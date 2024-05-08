d8541ae123c56f889386f69ae332b8b94ea92751
Akash Dey <dey899684@gmail.com>
2024-05-08 14:56:16 +0530

    test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,8 @@ import UserInfo from "./components/Userinfo";
 export default function Home() {
   return (
   <div className="grid place-items-center h-screen -mt-24">
-    <UserInfo />
+    <UserInfo />  
+    
   </div>
   );
 }
